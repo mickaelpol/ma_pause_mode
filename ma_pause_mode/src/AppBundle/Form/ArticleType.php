530c0447bfceffab938aff6674c6336301513280
micka <polmickael3@gmail.com>
2018-01-08 22:32:07 +0100

    save en cours de dev article ajout de date

--- ma_pause_mode/src/AppBundle/Form/ArticleType.php
+++ ma_pause_mode/src/AppBundle/Form/ArticleType.php
@@ -6,6 +6,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 
 class ArticleType extends AbstractType
@@ -15,14 +18,22 @@ class ArticleType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title', CKEditorType::class , array('config' => array(
-            'toolbar' => 'basic'
+        $builder->add('title', CKEditorType::class, array(
+            'label' => 'titre',
+            'config' => array(
+                'toolbar' => 'basic'
+            )
+        ))
+
+        ->add('content', CKEditorType::class, array(
+            'label' => 'contenu',
+            'config' => array(
+            'class' => 'ckeditor', 'class' => 'form-control',
         )))
-        ->add('content', CKEditorType::class, array('config' => array(
-            'class' => 'ckeditor'
-        )))
-        ->add('date')
-        ->add('dateUpdate');
+        ->add('description', TextType::class , array('label' => 'description', 'translation_domain' => 'FOSUserBundle'));
+        // ->add('ville', null, array('label' => 'Ville', 'translation_domain' => 'FOSUserBundle'));
+        // ->add('date')
+        // ->add('dateUpdate');
     }/**
      * {@inheritdoc}
      */
